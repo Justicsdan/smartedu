a7f60b72e8e026e6db4f1a04306dd7a1b0ea53f2
justicsdan <justicsd99@gmail.com>
2026-07-20 23:01:33 +0100

    Deploy build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "0d5a69ba369fddc932c059580a9362ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "660952e5d4f7f3d494c4a2e2684cc273",
+"flutter_bootstrap.js": "2ce5de88986843a2b25169413f43b159",
 "canvaskit/chromium/canvaskit.js": "a80c765aaa8af8645c9fb1aae53f9abf",
 "canvaskit/chromium/canvaskit.js.symbols": "e2d09f0e434bc118bf67dae526737d07",
 "canvaskit/chromium/canvaskit.wasm": "a726e3f75a84fcdf495a15817c63a35d",
