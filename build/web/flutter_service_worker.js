c3fdea888658e1dc2b5a6a5680cf88f9cef161ca
justicsdan <justicsd99@gmail.com>
2026-07-31 23:37:00 +0100

    Fix results table: add column borders for visual alignment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "0d5a69ba369fddc932c059580a9362ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "5d934fbb1574c5e2535706c00905e140",
+"flutter_bootstrap.js": "565c67aeb2fcbdea4f73bc586a01e873",
 "canvaskit/chromium/canvaskit.js": "a80c765aaa8af8645c9fb1aae53f9abf",
 "canvaskit/chromium/canvaskit.js.symbols": "e2d09f0e434bc118bf67dae526737d07",
 "canvaskit/chromium/canvaskit.wasm": "a726e3f75a84fcdf495a15817c63a35d",
